4bb53af45b4a78a925c420daaad34be08063e925
szt2bacpet <bacskaipeter01@gmail.com>
2023-11-12 16:57:25 +0100

    ya

--- osztalyzando/js.js
+++ osztalyzando/js.js
@@ -1,23 +1,116 @@
-// Az "Dark Mode" gomb elem kiválasztása
-const darkModeToggle = document.getElementById('dark-mode-toggle');
+const timetableData = [
+    {
+        day: "Hétfő",
+        lessons: [
+            { time: "8:00", subject: "Szakmai angol" },
+            { time: "8:55", subject: "Webprogramozás" },
+            { time: "9:55", subject: "Adatbázis kezelés" },
+            { time: "10:50", subject: "Mobilalkalmazás fejlesztés" },
+            { time: "11:45", subject: "Frontend fejlesztés" },
+            { time: "12:45", subject: "Backend fejlesztés" },
+            { time: "13:40", subject: "Java programozás" },
+        ]
+    },
+    {
+        day: "Kedd",
+        lessons: [
+            { time: "8:00", subject: "Javascript programozás" },
+            { time: "8:55", subject: "MVVM tervezés" },
+            { time: "9:55", subject: "Frentend fejlesztés" },
+            { time: "10:50", subject: "Backend fejlesztés" },
+            { time: "11:45", subject: "Java programozás" },
+        ]
+    },
+    {
+        day: "Szerda",
+        lessons: [
+            { time: "8:00", subject: "Szakmai angol" },
+            { time: "8:55", subject: "Webprogramozás" },
+            { time: "9:55", subject: "Adatbázis kezelés" },
+            { time: "10:50", subject: "Mobilalkalmazás fejlesztés" },
+            { time: "11:45", subject: "Frontend fejlesztés" },
+            { time: "12:45", subject: "Backend fejlesztés" },
+            { time: "13:40", subject: "Javascript programozás" },
+        ]
+    },
+    {
+        day: "Csütörtök",
+        lessons: [
+            { time: "8:00", subject: "Szakmai angol" },
+            { time: "8:55", subject: "Webprogramozás" },
+            { time: "9:55", subject: "Adatbázis kezelés" },
+            { time: "10:50", subject: "Mobilalkalmazás fejlesztés" },
+            { time: "11:45", subject: "Frontend fejlesztés" },
+            { time: "12:45", subject: "Backend fejlesztés" },
+            { time: "13:40", subject: "MVVM tervezés" },
+        ]
+    },
+    {
+        day: "Péntek",
+        lessons: [
+            { time: "8:00", subject: "Javascript programozás" },
+            { time: "8:55", subject: "Java programozás" },
+            { time: "9:55", subject: "Webprogramozás" },
+            { time: "10:50", subject: "Adatbázis kezelés" },
+            { time: "11:45", subject: "Frontend fejlesztés" },
+            { time: "12:45", subject: "Backend fejlesztés" },
+            { time: "13:40", subject: "MVVM tervezés" },
+        ]
+    },
+    {
+        day: "Szombat",
+        lessons: [{ time: "-", subject: "Tanítás nélküli nap" }]
+    },
+    {
+        day: "Vasárnap",
+        lessons: [{ time: "-", subject: "Tanítás nélküli nap" }]
+    },
+];
 
-// Az aktuális mód meghatározása a localStorage-ban tárolt érték alapján
+function renderTimetable() {
+    const container = document.getElementById('container');
+    const cardsContainer = document.getElementById('cardsContainer');
+    
+    cardsContainer.innerHTML = "";
+
+    timetableData.forEach(dayData => {
+        const dayDiv = document.createElement('div');
+        dayDiv.classList.add('day');
+
+        const dayNameDiv = document.createElement('div');
+        dayNameDiv.classList.add('day-name');
+        const dayNameHeading = document.createElement('h2');
+        dayNameHeading.textContent = dayData.day;
+        dayNameDiv.appendChild(dayNameHeading);
+
+        dayDiv.appendChild(dayNameDiv);
+
+        dayData.lessons.forEach(lesson => {
+            const lessonDiv = document.createElement('div');
+            lessonDiv.classList.add('card');
+            lessonDiv.textContent = `${lesson.time}\n${lesson.subject}`;
+            dayDiv.appendChild(lessonDiv);
+        });
+
+        cardsContainer.appendChild(dayDiv);
+    });
+}
+
+const darkModeToggle = document.getElementById('dark-mode-toggle');
 const currentMode = localStorage.getItem('mode');
 
-// Alapértelmezett mód beállítása, ha nincs tárolt érték
 if (currentMode === 'dark') {
     document.body.classList.add('dark-mode');
 }
 
-// Módváltás funkció
 darkModeToggle.addEventListener('click', () => {
-    // Ha jelenleg világos mód van beállítva, váltás sötét módra
     if (document.body.classList.contains('dark-mode')) {
         document.body.classList.remove('dark-mode');
-        localStorage.setItem('mode', 'light'); // Mód mentése a localStorage-ba
+        localStorage.setItem('mode', 'light'); 
     } else {
-        // Ha jelenleg sötét mód van beállítva, váltás világos módra
         document.body.classList.add('dark-mode');
-        localStorage.setItem('mode', 'dark'); // Mód mentése a localStorage-ba
+        localStorage.setItem('mode', 'dark'); 
     }
 });
+
+renderTimetable();
